11ffe058ab5d57bde02e2626aef266776c7b203d
Тимур Белов <timurbl574@gmail.com>
2020-11-14 21:35:05 +0300

    add fonts

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,7 @@ gulp.task("sprite", function () {
 });
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
+    "source/fonts/*.{woff,woff2}",
     "source/images/**",
     "source/js/**",
     "source/*.ico",
